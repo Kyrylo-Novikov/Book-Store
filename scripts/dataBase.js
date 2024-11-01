79de3348762634b561d198f1f468c4d1e130ea9d
Kyrylo Novikov <novikovkyrylo@gmail.com>
2024-11-01 15:42:48 +0100

    database JS and biggest part of the Templete

---
+++ scripts/dataBase.js
@@ -0,0 +1,255 @@
+let books = [
+    {
+        "name": "Die Geheimnisse des Ozeans",
+        "author": "Clara Meer",
+        "likes": 1250,
+        "liked": true,
+        "price": 19.99,
+        "publishedYear": 2018,
+        "genre": "Fantasy",
+        "comments": [
+            {
+                "name": "Leser123",
+                "comment": "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat."
+            },
+            {
+                "name": "Bookworm84",
+                "comment": "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat."
+            },
+            {
+                "name": "FantasyFanatic",
+                "comment": "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte."
+            },
+            {
+                "name": "SciFiGuru",
+                "comment": "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren."
+            },
+            {
+                "name": "NovelLover",
+                "comment": "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat."
+            }
+        ]
+    },
+    {
+        "name": "Der vergessene Pfad",
+        "author": "Maximilian Schwarz",
+        "likes": 980,
+        "liked": false,
+        "price": 14.50,
+        "publishedYear": 2021,
+        "genre": "Fantasy",
+        "comments": []
+    },
+    {
+        "name": "Die Farben des Himmels",
+        "author": "Laura Blau",
+        "likes": 1520,
+        "liked": true,
+        "price": 22.95,
+        "publishedYear": 2019,
+        "genre": "Romantik",
+        "comments": [
+            {
+                "name": "LeserPeter",
+                "comment": "Die Handlung war fesselnd und die Charaktere unglaublich lebendig dargestellt."
+            },
+            {
+                "name": "BookLover21",
+                "comment": "Ein romantisches Meisterwerk, das mich tief berührt und bewegt hat."
+            },
+            {
+                "name": "FantasyNerd",
+                "comment": "Fantastische Welten und epische Abenteuer - genau mein Geschmack!"
+            },
+            {
+                "name": "SciFiEnthusiast",
+                "comment": "Die Zeitreise-Elemente waren genial und haben die Story spannend gemacht."
+            },
+            {
+                "name": "ReadingAddict",
+                "comment": "Ein unvergessliches Buch, das mich auf eine magische Reise mitgenommen hat."
+            }
+        ]
+    },
+    {
+        "name": "Das Rätsel der Zeit",
+        "author": "Alexander Weiss",
+        "likes": 750,
+        "liked": false,
+        "price": 18.00,
+        "publishedYear": 2020,
+        "genre": "Science-Fiction",
+        "comments": [
+            {
+                "name": "BuchKenner",
+                "comment": "Ein spannendes Abenteuer, das mich von Anfang an mitgerissen hat."
+            },
+            {
+                "name": "LeseWurm",
+                "comment": "Die Liebesgeschichte war herzergreifend und wunderschön geschrieben."
+            }
+        ]
+    },
+    {
+        "name": "Der letzte Wächter",
+        "author": "Sabine Grün",
+        "likes": 1300,
+        "liked": true,
+        "price": 16.75,
+        "publishedYear": 2017,
+        "genre": "Fantasy",
+        "comments": []
+    },
+    {
+        "name": "Im Schatten des Mondes",
+        "author": "Philipp Silber",
+        "likes": 890,
+        "liked": false,
+        "price": 12.30,
+        "publishedYear": 2022,
+        "genre": "Science-Fiction",
+        "comments": [
+            {
+                "name": "BücherLiebhaber",
+                "comment": "Eine magische Reise durch eine faszinierende Fantasiewelt, absolut fesselnd."
+            },
+            {
+                "name": "Leseratte",
+                "comment": "Ein packender Science-Fiction-Roman, der mich zum Nachdenken gebracht hat."
+            }
+        ]
+    },
+    {
+        "name": "Jenseits der Sterne",
+        "author": "Oliver Schwarz",
+        "likes": 1450,
+        "liked": true,
+        "price": 21.00,
+        "publishedYear": 2015,
+        "genre": "Science-Fiction",
+        "comments": [
+            {
+                "name": "Leser123",
+                "comment": "Ein fesselndes Abenteuer, das mich von Anfang bis Ende mitgerissen hat."
+            }
+        ]
+    },
+    {
+        "name": "Das verborgene Königreich",
+        "author": "Elena Gold",
+        "likes": 920,
+        "liked": false,
+        "price": 17.50,
+        "publishedYear": 2020,
+        "genre": "Fantasy",
+        "comments": [
+            {
+                "name": "Bookworm92",
+                "comment": "Ein faszinierendes Buch, das mich von der ersten Seite an gefesselt hat."
+            }
+        ]
+    },
+    {
+        "name": "Liebe in Zeiten des Krieges",
+        "author": "Emilia Rot",
+        "likes": 1800,
+        "liked": true,
+        "price": 19.99,
+        "publishedYear": 2016,
+        "genre": "Romantik",
+        "comments": [
+            {
+                "name": "Bibliophile23",
+                "comment": "Die Fantasiewelt war so lebendig, ich konnte das Buch kaum aus der Hand legen."
+            },
+            {
+                "name": "StorySeeker",
+                "comment": "Eine unglaublich berührende Liebesgeschichte, die mich tief bewegt hat."
+            },
+            {
+                "name": "SciFiExplorer",
+                "comment": "Spannende Zukunftsvisionen und interessante Charaktere machten diesen Roman einzigartig."
+            }
+        ]
+    }
+]
+
+
+
+
+
+for (let index = 0; index < books.length; index++) {
+
+    let titleRef = document.getElementById("infos_content")
+
+    
+
+    let bookTitle = books[index].name
+    let bookPrice = books[index].price
+    let bookLikes = books[index].likes
+    let bookAuthor = books[index].author
+    let bookPubYear = books[index].publishedYear
+    let bookGenre = books[index].genre
+    let bookComments= books[index].comments
+    
+
+// erzeuge ein templet extra für die kommentare diese werden in einem extra array gespeichert und dieser wird dann dem templett mit gegeben
+//   forEach -> im grunde auf den punk - für Jeden einzelnen.
+    let commentArr=[];
+
+    bookComments.forEach(comment =>{
+        commentArr.push(
+                        `<div><div>${comment.name} </div>  <div>: ${comment.comment}</div></div>`
+                        )});
+    console.log(commentArr);
+    
+
+    titleRef.innerHTML = bookInfoTemplete(index,commentArr)
+}
+    
+    
+
+function bookInfoTemplete(index,commentArr) {
+   
+    return  `
+    
+                            <div>
+                                <h1 class="title">${books[index].name}</h1>
+                            </div>
+                            <img src="" alt="Bild eines Buchs">
+                            <div>
+                                <div class="price">${books[index].price}</div>
+                                <div class="likes">${books[index].likes}</div>
+                                <input type="checkbox" name="" class="liked">
+                            </div>
+                            <div>
+                            <div>
+                                <div>Author:</div>
+                                <div class="author">${books[index].author}</div>
+                            </div>
+                            <div>
+                                <div>Erscheinungsjahr:</div>
+                                <div class="publishedYear">${books[index].publishedYear}</div>
+                            </div>
+                            <div>
+                                <div>Genre:</div>
+                                <div class="genre">${books[index].genre}</div>
+                            </div>
+                            <div>
+                            <h3>Kommentare :</h3>
+                            <div>${commentArr}</div>
+                            </div>                     
+            `
+}
+
+
+
+
+
+
+
+
+
+    
+
+
